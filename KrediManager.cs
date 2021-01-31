958e2d823dee79353e6d2b4bfb7d7b0a3311b147
Enes Ceylan <70946655+enescylnn@users.noreply.github.com>
2021-01-31 16:05:21 +0300

    OOP3

---
+++ KrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager
+    {
+        //İnterfaceleri birbirinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanırız.
+        void Hesapla();
+        void BiseyYap();
+        
+    }
+}
